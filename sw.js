a792256d88db22590afdc42ef13d26e64ba71b2b
iven86 <ivenprvb@gmail.com>
2024-01-11 02:44:21 +0100

    Update PayPal

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704668748';
+const cacheName = 'chirpy-1704937343';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
