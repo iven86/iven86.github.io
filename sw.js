8c3b4955c0be849923012b57d1e1947060909775
iven <ivenprvb@gmail.com>
2024-06-08 03:38:33 +0200

    Update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1712696839';
+const cacheName = 'chirpy-1717809677';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
